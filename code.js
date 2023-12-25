d614e6c02db8bbde17700cd5d03016bd55f797a1
Muhammad Saad <saadmizri434@gmail.com>
2023-12-25 22:16:42 +0500

    Update code.js

--- code.js
+++ code.js
@@ -504,9 +504,9 @@ function showResults(currentData) {
   scoreContainer.innerHTML = `<div>You scored ${score} out of ${currentData.length}.</div>
   <div>Skipped Questions:&nbsp;${skippedquestions}</div>
 <div class="progress-container">
-<div class="progress-bar"><p id="scoreshow" style = "display: none; color: black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${Math.floor(percentage)}%</p></div></div>
+<div class="progress-bar"><p id="scoreshow" style = "display: none; color: black; padding-left: 40%; width: 98%;">${Math.floor(percentage)}%</p></div></div>
 <div class="Nprogress-container">
-  <div class="Nprogress-bar"><p id="Nscoreshow" style = "display: none; color: black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${Math.floor(Nscorepercentage)}%</p></div>
+  <div class="Nprogress-bar"><p id="Nscoreshow" style = "display: none; color: black; padding-left: 40%; width: 98%;">${Math.floor(Nscorepercentage)}%</p></div>
   </div> 
   <p id="storedAverage"></p>
   <button class='btn text-black' onclick="clearStorage()">Clear Storage</button>
